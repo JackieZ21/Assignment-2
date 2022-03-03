31228a6faf16e4eecaced6aa79daefd5fb600fc0
Jackie Zheng <j4ckmanz14@gmail.com>
2022-03-02 18:49:00 -0700

    fourth commit added some menus March 1,2022 Jackie

--- Assignment 2/src/store/application/AppDriver.java
+++ Assignment 2/src/store/application/AppDriver.java
@@ -1,11 +1,15 @@
 package store.application;
+
+import store.view.AppMenus;
+
 /**
  * This class contains the main method and runs the code
- * @author Jackie Zheng Obayda Daoud
- *March 1,2022
+ * 
+ * @author Jackie Zheng Obayda Daoud March 1,2022
  */
 public class AppDriver {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 	
-}
+
+	}
 }
--- Assignment 2/src/store/view/AppMenus.java
+++ Assignment 2/src/store/view/AppMenus.java
@@ -1,9 +1,65 @@
 package store.view;
+
+import java.util.Scanner;
+
 /**
  * this class contains the menus for the program
  * @author Jackie Zheng Obayda Daoud
  *March 1,2022
  */
 public class AppMenus {
-
+	/**
+	 * This method greets user and prints out their options
+	 * @return the user input number
+	 */
+	Scanner input;
+	public AppMenus(){
+		input=new Scanner(System.in);
+		
+	}
+	/**
+	 * This method greets user and prints out their options
+	 * with input validation to only input numbers
+	 * @return the user input number
+	 */
+	public int showMainMenu() {
+		int option;
+		System.out.println("How can we help you ?\n");
+		System.out.println("\t(1). Search  Inventory and Purchase Toy");
+		System.out.println("\t(2). Add new Toy");
+		System.out.println("\t(3). Remove Toy");
+		System.out.println("\t(4). Save and Exit\n");
+		System.out.print("Enter a number choice: ");
+		//prints out users options to choose from 
+		while(!input.hasNextInt()) {
+			input.next();
+			System.out.println("Please enter a valid choice");
+		}
+		option=input.nextInt();
+		return option;
+		
+	}
+	/**
+	 * This method shows user search options
+	 * with input validation to only input numbers
+	 * @return the user input number
+	 */
+	public int showSearchMenu() {
+		int option;
+		System.out.println("Find Toys With :\n");
+		System.out.println("\t(1). Serial Number(SN)");
+		System.out.println("\t(2). Toy name");
+		System.out.println("\t(3). Type of toy");
+		System.out.println("\t(4). Back to main menu\n");
+		System.out.print("Enter a number choice here: ");
+		//prints out users options to choose from 
+		while(!input.hasNextInt()) {
+			input.next();
+			System.out.println("Please enter a valid choice");
+		}
+		option=input.nextInt();
+		return option;
+		
+	}
+	
 }
