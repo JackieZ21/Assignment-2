88534dd59b09b4da2f9943de5d945657ba404b6f
Jackie Zheng <j4ckmanz14@gmail.com>
2022-03-04 13:11:41 -0700

    Commit 9 added comments for the methods in toys Class Jackie and Obayda March 4,2022

--- Assignment 2/src/mru/tsc/controller/Toys.java
+++ Assignment 2/src/mru/tsc/controller/Toys.java
@@ -8,106 +8,119 @@ import java.util.Scanner;
 public class Toys {
 
 	private ArrayList<Toy> toys = new ArrayList<>();
+
 	/**
-	 * this method loads the information from txt file 
-	 * and adds it to the array list 
+	 * this method loads the information from txt file and adds it to the array list
+	 * 
 	 * @throws FileNotFoundException
 	 */
 	public void load() throws FileNotFoundException {
-
+		// create the file and location
 		File inFile = new File("toys.txt");
+		// Scanner to go through the whole toys txt file
 		Scanner fileScanner = new Scanner(inFile);
-
+		// while loop scans through each line
 		while (fileScanner.hasNextLine()) {
+			// Stores the file scanner into a variable
 			String toyText = fileScanner.nextLine();
+			// variable called to the method
 			Toy toy = textToToy(toyText);
+			// add the line to the array list
 			toys.add(toy);
 		}
 
 	}
+
 	/**
-	 * This method figures out what classification a toy is 
+	 * This method figures out what classification a toy is
 	 * 
 	 * @param toyText is what kind of toy it is
 	 * @return
 	 */
 	private Toy textToToy(String toyText) {
+		// if the text is identified as a animal toy return it as a animal toy
 		if (isAnimal(toyText)) {
 			return new Animal();
-		}else if(isBoardgame(toyText)) {
+		} else if (isBoardgame(toyText)) {// if the text is identified as a Boardgame toy return it as a Boardgame toy
 			return new Boardgame();
-		}else if (isFigure(toyText)) {
+		} else if (isFigure(toyText)) {// if the text is identified as a Figure toy return it as a Figure toy
 			return new Figure();
-		}else if (isPuzzle(toyText)) {
+		} else if (isPuzzle(toyText)) {// if the text is identified as a Puzzle toy return it as a Puzzle toy
 			return new Puzzle();
-		}else {return null;}
+		} else {
+			return null;
+		}
 		// if its an Animal
 		// return an Animal object
 		// else if its a Boardgame
 		// return ....
-		
+
 	}
+
 	/**
-	 * This method uses the serial number to figure out 
-	 * if the toy is a puzzle
+	 * This method uses the serial number to figure out if the toy is a puzzle
+	 * 
 	 * @param toyText input serial number
 	 * @return the toy
 	 */
 	private boolean isPuzzle(String toyText) {
 		// Checks the serial number first digit if it is 4,5 or 6
-		//to signify if it is a Puzzle
-		if(toyText.charAt(0)==4||toyText.charAt(0)==5||toyText.charAt(0)==6) {
+		// to signify if it is a Puzzle
+		if (toyText.charAt(0) == 4 || toyText.charAt(0) == 5 || toyText.charAt(0) == 6) {
 			return true;
-		}else {
-		return false;	
+		} else {
+			return false;
 		}
 	}
+
 	/**
-	 * This method uses the serial number to figure out 
-	 * if the toy is a figure
+	 * This method uses the serial number to figure out if the toy is a figure
+	 * 
 	 * @param toyText input serial number
 	 * @return the toy
 	 */
 	private boolean isFigure(String toyText) {
 		// Checks the serial number first digit if it is 0 and 1
-		//to signify if it is a Figure
-		if(toyText.charAt(0)==0||toyText.charAt(0)==1) {
+		// to signify if it is a Figure
+		if (toyText.charAt(0) == 0 || toyText.charAt(0) == 1) {
 			return true;
-		}else {
-		return false;	
+		} else {
+			return false;
 		}
 	}
+
 	/**
-	 * This method uses the serial number to figure out 
-	 * if the toy is a board game
+	 * This method uses the serial number to figure out if the toy is a board game
+	 * 
 	 * @param toyText input serial number
 	 * @return the toy
 	 */
 	private boolean isBoardgame(String toyText) {
-		// Checks the serial number first digit if it is 7,8 or 9 
-		//to signify if it is a Boardgame
-		if(toyText.charAt(0)==7||toyText.charAt(0)==8||toyText.charAt(0)==9) {
+		// Checks the serial number first digit if it is 7,8 or 9
+		// to signify if it is a Boardgame
+		if (toyText.charAt(0) == 7 || toyText.charAt(0) == 8 || toyText.charAt(0) == 9) {
 			return true;
-		}else {
-		return false;	
+		} else {
+			return false;
 		}
-		
+
 	}
+
 	/**
-	 * This method uses the serial number to figure out 
-	 * if the toy is a Animal
+	 * This method uses the serial number to figure out if the toy is a Animal
+	 * 
 	 * @param toyText input serial number
 	 * @return the toy
 	 */
 	private boolean isAnimal(String toyText) {
-		// Checks the serial number first digit if it is 2 or 3 
-		//to signify if it is a animal
-		if(toyText.charAt(0)==2||toyText.charAt(0)==3) {
+		// Checks the serial number first digit if it is 2 or 3
+		// to signify if it is a animal
+		if (toyText.charAt(0) == 2 || toyText.charAt(0) == 3) {
 			return true;
-		}else {
-		return false;	
+		} else {
+			return false;
 		}
-		
+
 	}
 
 }
