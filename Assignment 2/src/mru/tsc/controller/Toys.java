2a4f73d5e99282ddabfb9f5b3f5f691a7bc8a940
Jackie Zheng <j4ckmanz14@gmail.com>
2022-03-04 12:29:40 -0700

    Commit 7 Obayda and Jackie Added new methods in Toys which identifies the toy category

--- Assignment 2/src/mru/tsc/controller/Toys.java
+++ Assignment 2/src/mru/tsc/controller/Toys.java
@@ -8,7 +8,11 @@ import java.util.Scanner;
 public class Toys {
 
 	private ArrayList<Toy> toys = new ArrayList<>();
-
+	/**
+	 * this method loads the information from txt file 
+	 * and adds it to the array list 
+	 * @throws FileNotFoundException
+	 */
 	public void load() throws FileNotFoundException {
 
 		File inFile = new File("toys.txt");
@@ -21,18 +25,64 @@ public class Toys {
 		}
 
 	}
-
+	/**
+	 * This method figures out what classification a toy is 
+	 * 
+	 * @param toyText is what kind of toy it is
+	 * @return
+	 */
 	private Toy textToToy(String toyText) {
 		if (isAnimal(toyText)) {
 			return new Animal();
-		}
+		}else if(isBoardgame(toyText)) {
+			return new Boardgame();
+		}else if (isFigure(toyText)) {
+			return new Figure();
+		}else if (isPuzzle(toyText)) {
+			return new Puzzle();
+		}else {return null;}
 		// if its an Animal
 		// return an Animal object
 		// else if its a Boardgame
 		// return ....
-		return null;
+		
 	}
-
+	/**
+	 * This method uses the serial number to figure out 
+	 * if the toy is a puzzle
+	 * @param toyText input serial number
+	 * @return the toy
+	 */
+	private boolean isPuzzle(String toyText) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	/**
+	 * This method uses the serial number to figure out 
+	 * if the toy is a figure
+	 * @param toyText input serial number
+	 * @return the toy
+	 */
+	private boolean isFigure(String toyText) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	/**
+	 * This method uses the serial number to figure out 
+	 * if the toy is a board game
+	 * @param toyText input serial number
+	 * @return the toy
+	 */
+	private boolean isBoardgame(String toyText) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	/**
+	 * This method uses the serial number to figure out 
+	 * if the toy is a Animal
+	 * @param toyText input serial number
+	 * @return the toy
+	 */
 	private boolean isAnimal(String toyText) {
 		// TODO Auto-generated method stub
 		return false;
